ccfcb471c21ba0376d4580985c9d15f5213a6b03
ohtpz <hidalgoc536@gmail.com>
2026-01-13 21:17:27 +0100

    Initialization de l'application
    Debut de l'index

---
+++ stagesinfo/app/(public)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "@/components/layout/navbar";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto sm:px-10 px-5 py-8">
+        <h1>Stages Disponible</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ stagesinfo/components/layout/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="italic">
+          <p className="text-3xl font-bold">StagesInfo</p>
+        </Link>
+        <ul>
+          <Link href="/" className="nav-link">
+            Offres
+          </Link>
+          <Link href="/connexion" className="nav-link text-chart-1">
+            Connexion
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
